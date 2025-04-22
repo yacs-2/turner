0426a07dae8de9c9d91411a5e5beb4db939b3640
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-04-22 14:37:26 -0400

    Update list.js

--- wdsub/list.js
+++ wdsub/list.js
@@ -1,6 +1,6 @@
 //These are the checkboxes for the <li>'s
 const UNCHECK_ICON = '<svg onclick="crossOut(this)" viewBox="0 0 448 512" width="2em" title="square-uncheck"> <path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32z" /> </svg>';
-const CHECK_ICON = '<svg onclick="uncrossOut(this)" viewBox="0 0 448 512" width="2em" title="square-check"> <path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" /> </svg>';
+const CHECK_ICON = '<svg onclick="unmarkItem(this)" viewBox="0 0 448 512" width="2em" title="square-check"> <path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM337 209L209 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L303 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" /> </svg>';
 
 //This is the function which adds a new <li>.
 function addNewItem() {
@@ -13,10 +13,11 @@ function addNewItem() {
   input = document.getElementById("new-item");
   newItemText = input.value;
   
-//Doesn't allow for blank spaces to be added
   if (newItemText != "") {
     newItem = document.createElement("li");
-    newItem.innerHTML = UNCHECK_ICON + "   " + newItemText;
+    newItem.innerHTML = UNCHECK_ICON + newItemText;
+    newItem.style.listStyleType = "none";
+    newItem.style.transform = "translate(0, 25px)";
     input.value = "";
 
     myList = document.getElementById("list-grp");
@@ -31,18 +32,15 @@ function clearList() {
   theList.innerHTML = "";
 }
 
-//This crosses out <li>'s
 function crossOut(_icon) {
   let thisItem = _icon.parentElement;
   thisItem.style.textDecoration = "solid 2px line-through";
-  thisItem.style.color = "gray";
   thisItem.innerHTML = CHECK_ICON + "   " + thisItem.innerText;
 }
 
-function uncrossOut(_icon) {
+function unmarkItem(_icon) {
   let thisItem = _icon.parentElement;
   thisItem.style.textDecoration = "none";
-  thisItem.style.color = "black";
   thisItem.innerHTML = UNCHECK_ICON + "   " + thisItem.innerText;
 }
 
