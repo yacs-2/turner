68a87e2b6b6e2657e95395f1db137ef1a6e81c7b
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-04-28 15:30:47 -0400

    Create photo-albums.js

---
+++ photo-albums/photo-albums.js
@@ -0,0 +1,10 @@
+function showBig(_self) {
+  const BIG_DIV = document.getElementById("big-container"); 
+  const BIG_IMG = document.getElementById("big-img");
+  
+  BIG_DIV.style.display = "block";
+  BIG_IMG.setAttribute("src", _self.src);
+}
+function hideBig(_self) {
+  _self.style.display = "none";
+}
